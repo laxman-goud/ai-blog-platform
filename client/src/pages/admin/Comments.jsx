d0f028a82710e44f1383cde070e602c23918249e
laxman-goud <codewithlaxman92@gmail.com>
2026-01-05 12:19:22 +0530

    client(feat): connect Comments page with backend endpoints

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,15 +1,23 @@
-/* eslint-disable react-hooks/set-state-in-effect */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
-import { comments_data } from '../../assets/assets'
 import CommentTableItem from '../../components/admin/CommentTableItem'
+import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast'
 
 const Comments = () => {
 
     const [comments, setComments] = useState([])
     const [filter, setFilter] = useState('Not Approved')
 
+    const {axios} = useAppContext()
+
     const fetchComments = async () => {
-        setComments(comments_data)
+        try {
+            const {data} = await axios.get('/api/admin/comments')
+            data.success ? setComments(data.comments) : toast.error(data.message)
+        } catch (error) {
+            toast.error(error.message)
+        }
     }
 
     useEffect(() => {
