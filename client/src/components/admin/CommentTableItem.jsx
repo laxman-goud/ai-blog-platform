c9d82e0f391f75f14078741a4ca589c774fc6e6b
laxman-goud <codewithlaxman92@gmail.com>
2026-01-05 12:18:20 +0530

    client(feat): connect CommentTableItem  component with backend endpoints

--- client/src/components/admin/CommentTableItem.jsx
+++ client/src/components/admin/CommentTableItem.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import { assets } from '../../assets/assets';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const CommentTableItem = ({comment, fetchComments}) => {
 
     const {blog, createdAt, _id} = comment;
     const BlogDate = new Date(createdAt)
 
+    const {axios} = useAppContext()
+
+    const approveComment = async () => {
+        try {
+            const {data} = await axios.post('/api/admin/approve-comment', {id: _id})
+            if(data.success){
+                await fetchComments()
+                toast.success(data.message)
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const deleteComment = async () => {
+        const confirm = window.confirm('Are you sure you want to delete this comment?')
+        if(!confirm) return;
+        try {
+            const {data} = await axios.post('/api/admin/delete-comment', {id: _id})
+            if(data.success){
+                await fetchComments()
+                toast.success(data.message)
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
 
     return (
         <tr className='order-y border-gray-300'>
@@ -24,14 +59,14 @@ const CommentTableItem = ({comment, fetchComments}) => {
                 <div className='inline-flex items-center gap-4'>
                     {
                         !comment.isApproved ? (
-                            <img src={assets.tick_icon} alt="tick" className='w-5 hover:scale-110 transition-all cursor-pointer' />
+                            <img onClick={approveComment} src={assets.tick_icon} alt="tick" className='w-5 hover:scale-110 transition-all cursor-pointer' />
                         ) : (
                             <p className='text-xs border border-green-600 bg-green-100 text-green-600 rounded-full px-3 py-1'>
                                 Approved
                             </p>
                         )
                     }
-                    <img src={assets.bin_icon} alt="bin" className='w-5 hover:scale-110 transition-all cursor-pointer' />
+                    <img onClick={deleteComment} src={assets.bin_icon} alt="bin" className='w-5 hover:scale-110 transition-all cursor-pointer' />
                 </div>
             </td>
             
