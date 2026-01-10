af3f380ae07b1e23c36c8ee50bb974114137dc2d
laxman-goud <codewithlaxman92@gmail.com>
2026-01-10 20:02:20 +0530

    client(docs): enhance code comments and formatting in App.jsx for better readability

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,55 @@
 import React from 'react'
+
+// Routing utilities
 import { Route, Routes } from 'react-router-dom'
+
+// Public pages
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+
+// Admin layout and pages
 import Layout from './pages/admin/Layout'
 import Dashboard from './pages/admin/Dashboard'
 import Comments from './pages/admin/Comments'
 import AddBlog from './pages/admin/AddBlog'
 import ListBlog from './pages/admin/ListBlog'
+
+// Admin login component
 import Login from './components/admin/Login'
+
+// Quill editor styles (used in AddBlog)
 import 'quill/dist/quill.snow.css'
-import {Toaster} from 'react-hot-toast'
+
+// Toast notifications
+import { Toaster } from 'react-hot-toast'
+
+// Global app context
 import { useAppContext } from './context/AppContext'
 
 const App = () => {
-  const {token} = useAppContext()
+  // Get auth token from context
+  const { token } = useAppContext()
 
   return (
     <div>
+      {/* Global toast notifications */}
       <Toaster />
+
+      {/* Application routes */}
       <Routes>
+        {/* Public routes */}
         <Route path='/' element={<Home />} />
         <Route path='/blog/:id' element={<Blog />} />
-        <Route path='/admin'  element={token ? <Layout />: <Login />}>
+
+        {/* Admin routes (protected by auth token) */}
+        <Route
+          path='/admin'
+          element={token ? <Layout /> : <Login />}
+        >
+          {/* Default admin dashboard */}
           <Route index element={<Dashboard />} />
+
+          {/* Admin sub-pages */}
           <Route path='comments' element={<Comments />} />
           <Route path='addBlog' element={<AddBlog />} />
           <Route path='listBlog' element={<ListBlog />} />
