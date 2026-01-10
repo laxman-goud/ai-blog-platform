b63eaee5528ca9201c444eb15e102fed7cbf2632
laxman-goud <codewithlaxman92@gmail.com>
2026-01-10 20:22:41 +0530

    client(docs): enhance comments and formatting in Loader.jsx

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+/**
+ * Loader
+ * Displays a centered spinning loader
+ * Used while fetching async data
+ */
 const Loader = () => {
     return (
         <div className='flex justify-center items-center h-screen'>
+            {/* Spinner animation */}
             <div className='animate-spin rounded-full h-16 w-16 border-4 border-t-white border-gray-700'></div>
-            
         </div>
     )
 }
