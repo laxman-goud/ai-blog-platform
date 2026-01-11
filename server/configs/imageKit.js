eb8d9cf480c385586cea8a236677257289cf392f
laxman-goud <codewithlaxman92@gmail.com>
2026-01-11 09:47:35 +0530

    server(docs): improve documentation comments in imageKit.js

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,9 +1,14 @@
 import ImageKit from 'imagekit'
 
-var imagekit = new ImageKit({
+/**
+ * ImageKit configuration
+ * - Handles blog image uploads
+ * - Optimized delivery via CDN
+ */
+const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 })
 
-export default imagekit;
+export default imagekit
